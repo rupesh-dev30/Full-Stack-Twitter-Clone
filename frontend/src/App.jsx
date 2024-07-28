8278674338ad6194e8cab51ca3e4618ca9d7b6e1
rupesh-dev30 <cgcrupesh@gmail.com>
2024-07-28 15:43:39 +0530

    basic UI done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,26 @@
+import { Route, Routes } from "react-router-dom";
+import SignUpPage from "./pages/auth/signup/SignUpPage";
+import LoginPage from "./pages/auth/login/LoginPage";
+import HomePage from "./pages/home/HomePage";
+import Sidebar from "./components/common/Sidebar";
+import RightPanel from "./components/common/RightPanel";
+import NotificationPage from "./pages/notification/NotificationPage";
+import ProfilePage from "./pages/profile/ProfilePage";
+
 function App() {
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hlo World</h1>
-      <button className="btn">Button</button>
-      <button className="btn btn-neutral">Neutral</button>
-      <button className="btn btn-primary">Primary</button>
-      <button className="btn btn-secondary">Secondary</button>
-      <button className="btn btn-accent">Accent</button>
-      <button className="btn btn-ghost">Ghost</button>
-      <button className="btn btn-link">Link</button>
-    </>
-  );
+	return (
+		<div className='flex max-w-6xl mx-auto'>
+			<Sidebar />
+			<Routes>
+				<Route path='/' element={<HomePage />} />
+				<Route path='/signup' element={<SignUpPage />} />
+				<Route path='/login' element={<LoginPage />} />
+				<Route path='/notifications' element={<NotificationPage />} />
+				<Route path='/profile/:username' element={<ProfilePage />} />
+			</Routes>
+			<RightPanel />
+		</div>
+	);
 }
 
 export default App;
