3382b4a15247dec1f8a79464bb5851838c2a9136
rupesh-dev30 <cgcrupesh@gmail.com>
2024-07-28 14:38:43 +0530

    daisyui setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,13 @@ function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hlo World</h1>
+      <button className="btn">Button</button>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-secondary">Secondary</button>
+      <button className="btn btn-accent">Accent</button>
+      <button className="btn btn-ghost">Ghost</button>
+      <button className="btn btn-link">Link</button>
     </>
   );
 }
