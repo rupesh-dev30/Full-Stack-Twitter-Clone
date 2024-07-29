0cfcd51eeea99d0a0aea9690512f46a8b61a44ba
rupesh-dev30 <cgcrupesh@gmail.com>
2024-07-30 01:54:13 +0530

    react query and signup in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Sidebar from "./components/common/Sidebar";
 import RightPanel from "./components/common/RightPanel";
 import NotificationPage from "./pages/notification/NotificationPage";
 import ProfilePage from "./pages/profile/ProfilePage";
+import {Toaster} from "react-hot-toast";
 
 function App() {
 	return (
@@ -19,6 +20,7 @@ function App() {
 				<Route path='/profile/:username' element={<ProfilePage />} />
 			</Routes>
 			<RightPanel />
+			<Toaster />
 		</div>
 	);
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,10 +4,16 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
