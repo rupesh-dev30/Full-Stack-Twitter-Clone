32e1a5d21e1b86c7583d1d4c9d24dda2bc49a3cd
rupesh-dev30 <cgcrupesh@gmail.com>
2024-08-03 17:18:42 +0530

    follow

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -41,7 +41,7 @@ export const followUnfollowUser = async (req, res) => {
       // currentUser.following = currentUser.following.filter((id) => id !== id);
       await User.findByIdAndUpdate(id, { $pull: { followers: req.user._id } });
       await User.findByIdAndUpdate(req.user._id, { $pull: { following: id } });
-      //TODO return the id of the user as a response
+
       res.status(200).json({ message: "User unfollowed successfully" });
     } else {
       //follow the user
@@ -57,7 +57,6 @@ export const followUnfollowUser = async (req, res) => {
 
       await newNotification.save();
 
-      //TODO return the id of the user as a response
       res.status(200).json({ message: "User followed successfully" });
 
       //send notifcation
