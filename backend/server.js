577c66ab362e4138e58e0db61f8f502d658685ff
rupesh-dev30 <cgcrupesh@gmail.com>
2024-08-13 23:31:26 +0530

    add build script, change in server js sendFile... and many more for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
@@ -18,14 +19,24 @@ cloudinary.config({
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
-app.use(express.json({limit: "5mb"})); // to parse req.body     //DOS
+app.use(express.json({ limit: "5mb" })); // to parse req.body     //DOS
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
